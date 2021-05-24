f0cd39199a1a194234ac445a7a2d22c7bb726053
iTeChNoLoGiCi <konovalovvlad@bk.ru>
2021-05-24 11:50:40 +0300

    Formatting

--- src/main/java/com/es/phoneshop/model/product/ProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ProductDao.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 public interface ProductDao {
     Product getProduct(Long id);
+
     List<Product> findProducts();
+
     void save(Product product);
+
     void delete(Long id);
 }
