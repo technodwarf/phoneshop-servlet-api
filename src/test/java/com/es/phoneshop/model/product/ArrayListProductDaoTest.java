071643c907e5b3505fd649fa6b90c87f7049f769
iTeChNoLoGiCi <konovalovvlad@bk.ru>
2021-05-24 16:51:39 +0300

    Add test for deleting product

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -31,5 +32,17 @@ public class ArrayListProductDaoTest {
         Product result = productDao.getProduct((Long.valueOf(product.getId())));
         assertNotNull(result);
         assertEquals("test-product", result.getCode());
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("test-product", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+
+        List<Product> products = productDao.findProducts();
+        assertTrue(products.contains(product));
+        productDao.delete(product.getId());
+        assertFalse(products.contains(product));
     }
 }
